ba32cad4235cd9d721e9a85251bf812b8cd24c57
MagedAhmad <maged.ahmed10@hotmail.com>
2020-07-02 19:56:46 +0200

    Fix installation

---
+++ app/Http/Controllers/Api/ComponentController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Component;
+use App\Http\Controllers\Controller;
+use App\Http\Resources\ComponentResource;
+use Illuminate\Http\Request;
+
+class ComponentController extends Controller
+{
+    public function index()
+    {
+        $components = ComponentResource::collection(
+                        Component::with('user')->where('private', 0)->get()
+                    );
+        
+        return json_encode($components);
+    }
+}
